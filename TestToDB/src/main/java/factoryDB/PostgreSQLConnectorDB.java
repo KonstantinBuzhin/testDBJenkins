d4e0113e6aa52c4152dee28b293a929db2941019
Konstantin Buzhin <k.buzhin.11@gmail.com>
2021-03-25 17:28:03 +0200

    got better code

--- TestToDB/src/main/java/factoryDB/PostgreSQLConnectorDB.java
+++ TestToDB/src/main/java/factoryDB/PostgreSQLConnectorDB.java
@@ -22,25 +22,10 @@ public class PostgreSQLConnectorDB implements ConnectorDB {
 
 	@Override
 	public List<User> getUsers() {
-		try {
-			Class.forName("org.postgresql.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		Connection connection = null;
-
-		try {
-			connection = DriverManager.getConnection(DB_URL, USER, PASS);
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		Statement stmt;
 		List<User> listUsers = new ArrayList<>();
-		try {
-			stmt = connection.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT * from users");
+		try (Connection connection = createConnection();
+				Statement stmt = connection.createStatement();
+				ResultSet rs = stmt.executeQuery("SELECT * from users")) {
 
 			while (rs.next()) {
 				User user = new User();
@@ -49,17 +34,7 @@ public class PostgreSQLConnectorDB implements ConnectorDB {
 				user.setAge(rs.getInt("age"));
 				listUsers.add(user);
 			}
-
-			rs.close();
-
-			stmt.close();
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-
-		try {
-			connection.close();
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
@@ -68,32 +43,12 @@ public class PostgreSQLConnectorDB implements ConnectorDB {
 
 	@Override
 	public void addUser(User user) {
-		try {
-			Class.forName("org.postgresql.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		Connection connection = null;
-
-		try {
-			connection = DriverManager.getConnection(DB_URL, USER, PASS);
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			PreparedStatement pst = connection.prepareStatement("INSERT INTO users(username, age) VALUES(?, ?)");
+		try (Connection connection = createConnection();
+				PreparedStatement pst = connection.prepareStatement("INSERT INTO users(username, age) VALUES(?, ?)");) {
 			pst.setString(1, user.getUsername());
 			pst.setInt(2, user.getAge());
 			pst.execute();
-			pst.close();
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
-		try {
-			connection.close();
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
@@ -102,31 +57,13 @@ public class PostgreSQLConnectorDB implements ConnectorDB {
 	@Override
 	public void getUserById(User user) {
 
-		try {
-			Class.forName("org.postgresql.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		Connection connection = null;
-
-		try {
-			connection = DriverManager.getConnection(DB_URL, USER, PASS);
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		Statement stmt;
-		try {
-			stmt = connection.createStatement();
-
+		try (Connection connection = createConnection();
+				Statement stmt = connection.createStatement();
+				CallableStatement proc = connection.prepareCall("{ ? = call updateIvan() }")) {
 			stmt.execute("CREATE OR REPLACE PROCEDURE updateIvan(theAge int) AS '" + " BEGIN "
 					+ "    UPDATE users SET age = theAge WHERE name = 'ivan';" + "    COMMIT; "
 					+ " END;' LANGUAGE plpgsql");
-			stmt.close();
 
-			// Procedure call.
-			CallableStatement proc = connection.prepareCall("{ ? = call updateIvan() }");
 			proc.registerOutParameter(1, Types.OTHER);
 			proc.execute();
 			ResultSet results = (ResultSet) proc.getObject(1);
@@ -134,14 +71,7 @@ public class PostgreSQLConnectorDB implements ConnectorDB {
 				// do something with the results...
 			}
 			results.close();
-			proc.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			connection.close();
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
@@ -149,30 +79,11 @@ public class PostgreSQLConnectorDB implements ConnectorDB {
 
 	@Override
 	public void removeUser(User user) {
-		try {
-			Class.forName("org.postgresql.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		Connection connection = null;
-
-		try {
-			connection = DriverManager.getConnection(DB_URL, USER, PASS);
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		try {
-			PreparedStatement pst = connection.prepareStatement("DELETE FROM users WHERE id = ?");
+		try (Connection connection = createConnection();
+				PreparedStatement pst = connection.prepareStatement("DELETE FROM users WHERE id = ?");) {
 			pst.setInt(1, user.getIdUser());
 			pst.execute();
-			pst.close();
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
-		try {
-			connection.close();
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
@@ -180,11 +91,42 @@ public class PostgreSQLConnectorDB implements ConnectorDB {
 
 	public List<User> sortUsers(String field, String howToChange) {
 		List<User> listUsers = new ArrayList<>();
+
+		String change = null;
+
+		if (howToChange.equals("increase")) {
+			change = "ASC";
+		} else if (howToChange.equals("decrease")) {
+			change = "DESC";
+		}
+
+		if (change != null) {
+			try (Connection connection = createConnection();
+					Statement st = connection.createStatement();
+					ResultSet rs = st.executeQuery("SELECT * FROM USERS ORDER BY " + field + " " + change);) {
+				while (rs.next()) {
+					User user = new User();
+					user.setIdUser(rs.getInt("id"));
+					user.setUsername(rs.getString("username"));
+					user.setAge(rs.getInt("age"));
+					listUsers.add(user);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		return listUsers;
+
+	}
+
+	public Connection createConnection() {
 		try {
 			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+
 		Connection connection = null;
 
 		try {
@@ -193,42 +135,7 @@ public class PostgreSQLConnectorDB implements ConnectorDB {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
-		String change = null;
-		
-		if (howToChange.equals("increase")) {
-			change = "ASC";
-		} else if (howToChange.equals("decrease")) {
-			change = "DESC";
-		}
-		
-		if (change != null) {
-			try {
-
-				Statement st = connection.createStatement();
-				ResultSet rs = st.executeQuery("SELECT * FROM USERS ORDER BY "+field+" "+change);
-				while (rs.next()) {
-					User user = new User();
-					user.setIdUser(rs.getInt("id"));
-					user.setUsername(rs.getString("username"));
-					user.setAge(rs.getInt("age"));
-					System.out.println(user);
-					listUsers.add(user);
-				}
-
-				rs.close();
-				st.close();
-			} catch (SQLException ex) {
-				ex.printStackTrace();
-			}
-		}
-		try {
-			connection.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return listUsers;
-
+		return connection;
 	}
 
 }
