f79cdcd572dd5e60385fafbd1df10bf38ce3ee83
Konstantin Buzhin <k.buzhin.11@gmail.com>
2021-03-28 12:15:15 +0300

    maybe final update

--- TestToDB/src/main/java/commandPattern/AddAction.java
+++ TestToDB/src/main/java/commandPattern/AddAction.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import services.HomePageService;
+
+import services.AddingPageService;
 import services.PageService;
 
 public class AddAction implements Action {
 
-	private final PageService service = new HomePageService();
+	private final PageService service = new AddingPageService();
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
